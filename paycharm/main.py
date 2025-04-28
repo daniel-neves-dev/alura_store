6713f5f96cdf422450bb171c55f0035d4cfe91a0
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2025-04-28 17:47:08 -0300

    feature: feature store fare

---
+++ paycharm/main.py
@@ -0,0 +1,6 @@
+from store_fare import plot_store_fare_table, df_store_fare, plot_store_fare_bar_plot
+
+#------------------ Store fare cost ---------------------
+
+plot_store_fare_table(df_store_fare)
+plot_store_fare_bar_plot(df_store_fare)
