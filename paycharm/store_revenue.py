ac73bd1a99012681a8f998754957e33f7ea40143
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2025-04-28 13:26:57 -0300

    feature: revenue bar graph

--- paycharm/store_revenue.py
+++ paycharm/store_revenue.py
@@ -1,11 +1,11 @@
-from packages import pd, PrettyTable
+from packages import pd, plt, PrettyTable
 from load_stores import stores
 
 revenues = {store_name: (sum(df['Preço'])) for store_name, df in stores.items()}
 df_revenue = (pd.DataFrame(revenues.items(), columns=['Loja', 'Preço'])
                .rename(columns={'Preço':'Faturamento'}))
 
-def plot_tabel_revenue(revenues_df: pd.DataFrame):
+def plot_tabel_revenue(revenue_df: pd.DataFrame):
     """ Plot the total revenues for all stores """
 
     """
@@ -13,7 +13,7 @@ def plot_tabel_revenue(revenues_df: pd.DataFrame):
     """
 
     tabel = PrettyTable(['Loja', 'Faturamento'])
-    for _, row in revenues_df.iterrows():
+    for _, row in revenue_df.iterrows():
         tabel.add_row([
             row['Loja'],
             f"R${row['Faturamento']:,.2f}".replace(',', 'x').replace('.', ',').replace('x', '.')
@@ -26,4 +26,22 @@ def plot_tabel_revenue(revenues_df: pd.DataFrame):
     print('\n' + title.center(width))
     print(tabel_str)
 
+def plot_revenue_bar_graph(revenue_df: pd.DataFrame):
+    fig, axs = plt.subplots(figsize=(10,10))
+    bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange']
 
+    labels = [lbl for lbl in revenue_df['Loja']]
+    values = [va/1e6 for va in revenue_df['Faturamento']]
+
+
+    bars = axs.bar(labels, values, color = bar_colors, width=0.5)
+    axs.bar_label(bars, labels=[f'{vl:.2f} M' for vl in values], fontsize = 12)
+    axs.set_title('Faturamento total de cada loja em milhões', fontsize = 12, fontweight= 'bold')
+
+    axs.set_xticks(range(len(labels)))
+    axs.set_xticklabels(labels, fontsize = 12)
+
+    axs.yaxis.set_visible(False)
+    axs.spines[['top', 'left', 'right']].set_visible(False)
+
+    plt.show()
